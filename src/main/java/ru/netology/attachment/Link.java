7de35f0da77bfe08f04920fe134660f6ad18b8bb
viktoriya maslennikova <libikvikaa@yandex.ru>
2020-11-27 18:35:20 +0500

    Update

--- src/main/java/ru/netology/attachment/Link.java
+++ src/main/java/ru/netology/attachment/Link.java
@@ -10,4 +10,76 @@ public class Link {
     private Object button;
     private String previewPage;
     private  String previewUrl;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCaption() {
+        return caption;
+    }
+
+    public void setCaption(String caption) {
+        this.caption = caption;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Object getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(Object photo) {
+        this.photo = photo;
+    }
+
+    public Object getProduct() {
+        return product;
+    }
+
+    public void setProduct(Object product) {
+        this.product = product;
+    }
+
+    public Object getButton() {
+        return button;
+    }
+
+    public void setButton(Object button) {
+        this.button = button;
+    }
+
+    public String getPreviewPage() {
+        return previewPage;
+    }
+
+    public void setPreviewPage(String previewPage) {
+        this.previewPage = previewPage;
+    }
+
+    public String getPreviewUrl() {
+        return previewUrl;
+    }
+
+    public void setPreviewUrl(String previewUrl) {
+        this.previewUrl = previewUrl;
+    }
 }
